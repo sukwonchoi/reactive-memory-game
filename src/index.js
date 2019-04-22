9675813a90060622d2007d4f1b4511b97134c23c
Kevin Choi <kevinc@olympic.co.nz>
2019-04-22 20:19:19 +1200

    dynamically generate the cards on board and removed css module option

--- src/index.js
+++ src/index.js
@@ -1,5 +1,22 @@
 import { fromEvent } from 'rxjs';
 
-fromEvent(document.getElementById('click-me'), 'click').subscribe(() => {
-  alert('clicked');
+const boardEl = document.getElementById('board');
+
+fromEvent(boardEl, 'click', 'div.card').subscribe(e => {
+  console.log(e.target);
 });
+
+const generateCardEl = code => {
+  const card = document.createElement('div');
+  card.className = 'card';
+  card.innerHTML = code;
+  card.setAttribute('value', code);
+  return card;
+};
+
+const appendCardToBoard = (card, board = boardEl) => {
+  boardEl.appendChild(card);
+};
+
+// hardcode 9 for now
+[1, 2, 3, 4, 5, 6, 7, 8, 9].forEach(i => appendCardToBoard(generateCardEl(i)));
--- webpack.config.js
+++ webpack.config.js
@@ -24,7 +24,6 @@ module.exports = {
           {
             loader: 'css-loader',
             options: {
-              modules: true,
               importLoaders: 1,
               localIdentName: '[name]_[local]_[hash:base64]',
               sourceMap: true
