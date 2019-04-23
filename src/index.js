6d644ecc239f587ec488f4c2b58fbe004faaf4ef
Kevin Choi <kevinc@olympic.co.nz>
2019-04-23 20:19:56 +1200

    basic flipping and matching works

--- src/index.js
+++ src/index.js
@@ -1,16 +1,51 @@
 import { fromEvent } from 'rxjs';
+import { filter, map, delay, tap, bufferCount, distinctUntilChanged } from 'rxjs/operators';
+
+const CARD_VALUE_ATTRIBUTE = 'data-card-value';
 
 const boardEl = document.getElementById('board');
 
-fromEvent(boardEl, 'click', 'div.card').subscribe(e => {
-  console.log(e.target);
+const onCardClick$ = fromEvent(boardEl, 'click');
+const cardValue$ = onCardClick$.pipe(
+  filter(e => e.target.className === 'card-front' || e.target.className === 'card-back'),
+  map(e => e.path[2]), // hacky way to find the 'card' element. Figure out better way
+  distinctUntilChanged() // so you don't double click the same element
+);
+const cardPairs$ = cardValue$.pipe(bufferCount(2));
+
+cardValue$.subscribe(e => e.classList.toggle('active'));
+
+cardPairs$.pipe(delay(500)).subscribe(([firstCard, secondCard]) => {
+  if (firstCard.getAttribute(CARD_VALUE_ATTRIBUTE) === secondCard.getAttribute(CARD_VALUE_ATTRIBUTE)) {
+    firstCard.style.visibility = 'hidden';
+    secondCard.style.visibility = 'hidden';
+  } else {
+    firstCard.classList.toggle('active');
+    secondCard.classList.toggle('active');
+  }
 });
 
 const generateCardEl = code => {
   const card = document.createElement('div');
+  card.setAttribute(CARD_VALUE_ATTRIBUTE, code);
   card.className = 'card';
-  card.innerHTML = code;
-  card.setAttribute('value', code);
+
+  const cardContainer = document.createElement('div');
+  cardContainer.setAttribute(CARD_VALUE_ATTRIBUTE, code);
+  cardContainer.className = 'card-container';
+
+  const cardBack = document.createElement('div');
+  cardBack.className = 'card-back';
+  cardBack.innerHTML = code;
+
+  const cardFront = document.createElement('div');
+  cardFront.className = 'card-front';
+  cardFront.innerHTML = '?';
+
+  cardContainer.appendChild(cardBack);
+  cardContainer.appendChild(cardFront);
+
+  card.appendChild(cardContainer);
   return card;
 };
 
@@ -19,4 +54,4 @@ const appendCardToBoard = (card, board = boardEl) => {
 };
 
 // hardcode 9 for now
-[1, 2, 3, 4, 5, 6, 7, 8, 9].forEach(i => appendCardToBoard(generateCardEl(i)));
+[1, 2, 3, 3, 2, 1].forEach(i => appendCardToBoard(generateCardEl(i)));
