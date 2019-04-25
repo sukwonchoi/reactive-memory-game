2aa59b113ff24b25ebadcdfc607c5c1de5db741d
sukwonchoi <kswc95@gmail.com>
2019-04-25 21:44:11 +1200

    added basic reset logic

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
-import { fromEvent } from 'rxjs';
-import { filter, map, delay, tap, bufferCount, distinctUntilChanged } from 'rxjs/operators';
+import { fromEvent, of, Subject, BehaviorSubject } from 'rxjs';
+import { filter, map, tap, delay, bufferCount, distinctUntilChanged, scan, withLatestFrom } from 'rxjs/operators';
 import { generateCardEl, appendCardToBoard } from './html-render';
 import { CARD_VALUE_ATTRIBUTE } from './constants';
 import { shuffle } from './card-util';
+
 const boardEl = document.getElementById('board');
+const array = [1, 2, 3, 3, 2, 1];
+
+const score$ = new BehaviorSubject(0);
+const shuffledCards$ = new Subject();
+const shuffledCardElements$ = shuffledCards$.pipe(map(shuffledCards => shuffledCards.map(generateCardEl)));
 
 const onCardClick$ = fromEvent(boardEl, 'click');
 const cardValue$ = onCardClick$.pipe(
@@ -15,16 +21,34 @@ const cardPairs$ = cardValue$.pipe(bufferCount(2));
 
 cardValue$.subscribe(e => e.classList.toggle('active'));
 
-cardPairs$.pipe(delay(500)).subscribe(([firstCard, secondCard]) => {
-  if (firstCard.getAttribute(CARD_VALUE_ATTRIBUTE) === secondCard.getAttribute(CARD_VALUE_ATTRIBUTE)) {
-    firstCard.style.visibility = 'hidden';
-    secondCard.style.visibility = 'hidden';
-  } else {
-    firstCard.classList.toggle('active');
-    secondCard.classList.toggle('active');
-  }
-});
+cardPairs$
+  .pipe(
+    delay(500),
+    tap(([firstCard, secondCard]) => {
+      if (firstCard.getAttribute(CARD_VALUE_ATTRIBUTE) === secondCard.getAttribute(CARD_VALUE_ATTRIBUTE)) {
+        firstCard.style.visibility = 'hidden';
+        secondCard.style.visibility = 'hidden';
+      } else {
+        firstCard.classList.toggle('active');
+        secondCard.classList.toggle('active');
+      }
+    }),
+    filter(
+      ([firstCard, secondCard]) =>
+        firstCard.getAttribute(CARD_VALUE_ATTRIBUTE) === secondCard.getAttribute(CARD_VALUE_ATTRIBUTE)
+    ),
+    withLatestFrom(score$)
+  )
+  .subscribe(([_, score]) => {
+    if (score === 2) {
+      // basic reset logic
+      score$.next(0);
+      shuffledCards$.next(shuffle(array));
+    } else {
+      score$.next(++score);
+    }
+  });
 
-// TODO: Randomize these based on drop down(?) or difficulty
-const array = [1, 2, 3, 3, 2, 1];
-shuffle(array).forEach(i => appendCardToBoard(generateCardEl(i), boardEl));
+shuffledCardElements$.subscribe(cards => cards.forEach(c => appendCardToBoard(c, boardEl)));
+
+shuffledCards$.next(shuffle(array));
